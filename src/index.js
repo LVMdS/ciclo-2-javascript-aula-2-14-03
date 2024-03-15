ed154631eb78338985cf6e077461058721fffe28
LVMdS <leo92vinicius@gmail.com>
2024-03-15 19:48:38 -0300

    Respostas exercicios

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 // O que acontece ao tentar reatribuir um valor a const idade? ou seja, criar uma segunda const idade = 30?
 const idade = 25;
+const idade = 30;
 //console.log(idade); 
 
+//Uncaught TypeError: Assignment to constant variable. Isso ocorre porque uma vez que uma constante é declarada e inicializada, ela não pode ser alterada
 
 
 
@@ -20,7 +22,7 @@ const idade = 25;
 }
 // console.log(x);
 
-
+//Quando você tenta acessar x fora do bloco onde foi declarado, ocorrerá um erro, indicando que x não está definido. Isso ocorre porque x foi declarado com let e está limitado ao escopo do bloco em que foi definido.
 
 ///////////////////////////////////////////////////////////////////////
 
@@ -34,6 +36,8 @@ const idade = 25;
 // console.log(x);
 // console.log(y);
 
+//Quando você tentar imprimir x, ocorrerá um erro indicando que x não está definida. No entanto, quando você imprimir y, funcionará corretamente, pois y foi declarada com var e tem escopo global.
+
 ///////////////////////////////////////////////////////////////////////
 
 //O que acontece nessa declaração, explique sobre o Hoisting
@@ -43,6 +47,8 @@ var a = 5;
 console.log(b);
 //let b = 10;
 
+//Portanto, quando você tenta acessar a antes de atribuir um valor a ela (console.log(a)), a variável a já foi declarada (por causa do hoisting), mas ainda não foi atribuída, então o resultado será undefined.
+//quando você tenta acessar b antes de declará-la (console.log(b)), isso não funcionará como esperado. Isso ocorre porque variáveis declaradas com let e const não sofrem hoisting na fase de inicialização, apenas na fase de declaração. Então, quando o JavaScript chega à linha console.log(b), b ainda não foi declarada, resultando em um erro do tipo "ReferenceError: b is not defined"
 
 /////////////////////////////////////////////////////////////////////////////////
 
@@ -56,17 +62,21 @@ let y = 15;
 let y = 20; 
 // console.log(y); 
 
+//Na redeclaração de x, não há erro, pois var permite isso. Porém, na redeclaração de y, ocorrerá um erro porque let não permite a redeclaração de variáveis no mesmo escopo. O erro será do tipo "SyntaxError: Identifier 'y' has already been declared".
+
 
 //// Escreva um programa que print a primeira letra dessa variavel 
-let nomeCompleto = 'eduardo'
+let nomeCompleto = 'eduardo';
 
 //sua lógica para pegar a primeira letra 
-/// const letraInicial = ...
-//console.log(letraInicial)
+const letraInicial = nomeCompleto.charAt(0);
+console.log(letraInicial);
+// ou
+const letraInicial = nomeCompleto[0];
+console.log(letraInicial);
 
 
-
-
+//A primeira letra de uma string em JavaScript é acessada pela posição 0, já que a indexação começa em 0. Portanto, para obter a primeira letra da variável nomeCompleto, é utilizada a expressão nomeCompleto.charAt(0) ou nomeCompleto[0].
 
 
 
